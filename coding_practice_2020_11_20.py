4a20a444ecdebafc734dc4e7cc807a75a433ed63
KiSpence <74620015+KiSpence@users.noreply.github.com>
2020-11-20 13:45:33 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,2 +1,29 @@
+#1
 colour = input("What is your favourite colour? ")
-print("I also like", colour)
+print("Oh,",colour,"? Nice choice.")
+print("\n")
+
+#2
+c = input("How many cans are in the package? ")
+d = input("How many packages do you have? ")
+c = int(c)
+d = int(d)
+print("You should have",c * d,"cans in total.")
+print("\n")
+
+#3
+l = input ("What is the length of the prism? ")
+w = input ("What is the width of the prism? ")
+h = input ("What is the height of the prism? ")
+l = int (l)
+w = int (w)
+h = int (h)
+print("The prism is",l*w*h, "units cubed.")
+print("\n")
+
+#4
+question = input("Did you mute the teacher when you joined the Google Meet? ")
+if question == "Yes": 
+	print("That's probably not a good idea.")
+if question == "No":
+	print("Okay. Good.")
