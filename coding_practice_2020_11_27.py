7c4b4a4318820a43188543e62c3947d3b9365aa9
KiSpence <74620015+KiSpence@users.noreply.github.com>
2020-11-27 15:57:31 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,77 @@
+#3 to 97 by 2
+t = 3
+while t <= 97:
+	print(t)
+	t += 2
+
+print(" ")
+
+#-85 to 80 by 5
+q = -85
+while q <= 80:
+	print(q)
+	q += 5
+
+print(" ")
+
+#12 to 207 by 13
+a = 12
+while a <= 207:
+	print(a)
+	a += 13
+
+print(" ")
+
+#-190 to 354 by 16
+p = -190
+while p <= 354:
+	print(p)
+	p += 16
+
+print(" ")
+
+#435 to 540 by 3
+a = 435
+while a <= 540:
+	print(a)
+	a += 3
+
+print(" ")
+
+#-340 to -297 by 1
+l = -340
+while l <= -297:
+	print(l)
+	l += 1
+
+print(" ")
+
+#486 to 930 by 12
+g = 486
+while g <= 930:
+	print(g)
+	g += 12
+
+print(" ")
+
+#470 to 1128 by 14
+m = 470
+while m <= 1128:
+	print(m)
+	m += 14
+
+print(" ")
+
+#-118 to 314 by 16
+f = -118
+while f <= 314:
+	print(f)
+	f += 16
+
+print(" ")
+
+#297 to 671 by 17
+u = 297
+while u <= 671:
+	print(u)
+	u += 17
